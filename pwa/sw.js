731072dee187182c0faa73e1811871fdcada0fac
blackberrythangbv-bit <blackberrythangbv@gmail.com>
2026-09-11 10:31:52 +0700

    Add iPhone PWA V1.2 shell

---
+++ pwa/sw.js
@@ -0,0 +1,13 @@
+const CACHE='cbg-pwa-v12-20260911';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));
+});
